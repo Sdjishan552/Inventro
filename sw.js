0332a66860bd6bc8264d637b4350d2a176341c43
Syed Jishan Nawaz <sdjishan52@gmail.com>
2026-09-17 23:24:03 +0530

    Update cache version and core files in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'inventro-shell-v77';
-const CORE = ['./','./index.html','./style.css?v=73','./app.js?v=77','./manifest.json'];
+const CACHE = 'inventro-shell-v78';
+const CORE = ['./','./index.html','./style.css?v=74','./app.js?v=78','./manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
