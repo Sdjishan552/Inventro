0aa1d9b6dd0db130ac35ef7ce580717a3cf6e78c
Syed Jishan Nawaz <sdjishan52@gmail.com>
2026-09-17 23:36:49 +0530

    Update cache version and core files in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'inventro-shell-v78';
-const CORE = ['./','./index.html','./style.css?v=74','./app.js?v=78','./manifest.json'];
+const CACHE = 'inventro-shell-v79';
+const CORE = ['./','./index.html','./style.css?v=75','./app.js?v=79','./manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
