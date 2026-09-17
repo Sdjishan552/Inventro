eacffcf3b55db42d87ee809c45dee5cd95bf3b1b
Syed Jishan Nawaz <sdjishan52@gmail.com>
2026-09-17 22:50:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'inventro-shell-v76';
-const CORE = ['./','./index.html','./style.css?v=73','./app.js?v=76','./manifest.json'];
+const CACHE = 'inventro-shell-v77';
+const CORE = ['./','./index.html','./style.css?v=73','./app.js?v=77','./manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
